656a176429e334e965dd7ef56238ca200e3f8940
jackwonka <lstephane333@gmail.com>
2020-10-21 11:16:22 +0200

    Ajout code js / API

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-let elt = document.getElementById('main');
-elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
-elt.style.backgroundColor = "#000";
+//////////  Récupération des données ours en peluches avec l'API fetch, fonctionne avec les promesses.
+
+fetch('http://localhost:3000/api/teddies')
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+
+  // function avec message d'erreur du serveur HS
+  .catch(function(err){
+    console.log(err);
+    if(err === 0){ // requete annulée
+        alert("serveur HS");
+    }
+});
+
+////////// Afficher dynamiquement les produits sur la page d’accueil.
+
+// Création div sur parents bear
+
+let divItem = document.createElement("div");
+divItem.classList.add("item");
+document.getElementById("bear").appendChild(divItem);
+
+
+
+
